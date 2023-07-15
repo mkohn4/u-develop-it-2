3f4120b78ec65e456421482ed47f214ed2d9966d
Max Kohn <mkohn@mkohn-ltm4.internal.salesforce.com>
2023-07-15 11:24:32 -0400

    added parties table and joined tables in sql for get routes

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
+/*drop tables if they already exist to prevent db issues*/
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+/*parties table must be declared before candidates because candidates
+references parties with a foreign key*/
 CREATE TABLE parties (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
@@ -8,5 +13,8 @@ CREATE TABLE candidates(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    industry_connected BOOLEAN NOT NULL
+    party_id INTEGER,
+    industry_connected BOOLEAN NOT NULL,
+    /*foreign key constraint referencing id of parties table on delete set to null for candidate*/
+    CONSTRAINT fk_party FOREIGN KEY(party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
