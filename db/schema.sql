8991c403f7f8528a815ee3dee4c146163445a71f
Max Kohn <mkohn@mkohn-ltm4.internal.salesforce.com>
2023-07-16 12:30:35 -0400

    added voters table

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 /*drop tables if they already exist to prevent db issues*/
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
 /*parties table must be declared before candidates because candidates
 references parties with a foreign key*/
 CREATE TABLE parties (
@@ -17,4 +18,12 @@ CREATE TABLE candidates(
     industry_connected BOOLEAN NOT NULL,
     /*foreign key constraint referencing id of parties table on delete set to null for candidate*/
     CONSTRAINT fk_party FOREIGN KEY(party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
+
+CREATE TABLE voters (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
